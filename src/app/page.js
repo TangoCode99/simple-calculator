f2ce1702a7441f1a0961040d69e00e19beedb1e6
TangoCode99 <jnham7@gmail.com>
2024-11-14 13:40:07 -0500

    Title styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold mb-10">Simple Calculator</h1>
     </div>
   );
